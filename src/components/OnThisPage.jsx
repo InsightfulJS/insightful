9fb1100088715e01d591e4dbbfa8f87dcd84acc4
James Erringham-Bruce <james@qcutapp.com>
2023-10-09 16:23:14 +0100

    ✨ feat(scroll): highlight page heading on scroll

--- src/components/OnThisPage.jsx
+++ src/components/OnThisPage.jsx
@@ -1,8 +1,41 @@
-import React from 'react';
+'use client';
+
+import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
+import clsx from 'clsx';
+
 import ScrollToTopButton from './ScrollToTopButton';
 
 const OnThisPage = ({ headings = [] }) => {
+	const [activeHeading, setActiveHeading] = useState(null);
+
+	useEffect(() => {
+		const contentWindow = document.getElementById('content');
+		const headings = contentWindow.querySelectorAll('h2, h3, h4');
+
+		const handleScroll = () => {
+			let currentlyInView = null;
+
+			for (const heading of headings) {
+				const rect = heading.getBoundingClientRect();
+				if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
+					currentlyInView = heading;
+					console.log(heading);
+					break;
+				}
+			}
+
+			setActiveHeading(currentlyInView?.id);
+		};
+
+		handleScroll(); // on load before scrolling has occured
+		contentWindow.addEventListener('scroll', handleScroll);
+
+		return () => {
+			contentWindow.removeEventListener('scroll', handleScroll);
+		};
+	}, []);
+
 	return (
 		<nav className='w-56 flex flex-col justify-start items-start gap-4'>
 			<h2 className='text-sm font-semibold text-slate-900 dark:text-slate-100'>
@@ -13,13 +46,28 @@ const OnThisPage = ({ headings = [] }) => {
 					return (
 						<ol key={id} className='mt-2 space-y-3'>
 							<li>
-								<Link href={`#${id}`}>{title}</Link>
+								<Link
+									href={`#${id}`}
+									className={clsx(
+										activeHeading == id &&
+											'text-sky-700 dark:text-sky-600'
+									)}
+								>
+									{title}
+								</Link>
 								{children.length > 0 && (
 									<ol className='mt-2 space-y-3 pl-5'>
 										{children.map(({ id, title }) => {
 											return (
 												<li key={id}>
-													<Link href={`#${id}`}>
+													<Link
+														href={`#${id}`}
+														className={clsx(
+															activeHeading ==
+																id &&
+																'text-sky-700 dark:text-sky-600'
+														)}
+													>
 														{title}
 													</Link>
 												</li>
